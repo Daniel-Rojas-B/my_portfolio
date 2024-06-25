15f4b1d731ed6f8f9feed4d3d11fb975162137a3
Daniel R <152226403+Daniel-Rojas-B@users.noreply.github.com>
2024-06-24 22:35:18 -0700

    Commit

--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+  <Router basename={process.env.PUBLIC_URL}>
+    <App />
+  </Router>
+);
 
 
