7baf7f140478fb7941d3a58034b95a79dbe2f57f
Daniel R <152226403+Daniel-Rojas-B@users.noreply.github.com>
2024-06-24 19:42:55 -0700

    third commit

--- src/index.js
+++ src/index.js
@@ -6,12 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
